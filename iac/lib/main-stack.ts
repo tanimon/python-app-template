77d772a947ab0467ea5cda031fa721fc58cc4348
tanimon <8575113+tanimon@users.noreply.github.com>
2024-05-02 23:12:22 +0900

    Remove unnecessary comments

--- iac/lib/main-stack.ts
+++ iac/lib/main-stack.ts
@@ -1,16 +1,8 @@
-import * as cdk from "aws-cdk-lib";
+import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class MainStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class MainStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'IacQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
